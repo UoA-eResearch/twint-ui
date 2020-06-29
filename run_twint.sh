e533f3d4b280fd315610f21a9aff0e60d5bbeff3
Nick Young <nick.young@auckland.ac.nz>
2020-06-29 14:29:25 +1200

    don't breakup user timeline search queries

--- run_twint.sh
+++ run_twint.sh
@@ -15,16 +15,21 @@ cp header.csv "csvs/$filename.csv"
 
 pgrep -f "run_twint.sh $query" | grep -v "$$" | xargs kill -9
 
-while [[ $start -lt $end ]]
-do
-        bd=$(date -d"$end - 1 day" +"%Y-%m-%d")
-        ed=$(date -d"$end" +"%Y-%m-%d")
-	if [ -f "csvs/$filename/$bd.csv" ]; then
-		echo "$bd already done, skipping"
-	else
-        	echo "Running $query for $bd"
-	        twint -s "$query" --csv --output "csvs/$filename/$bd.csv" --since "$bd" --until "$ed" --proxy-host tor >> "logs/$filename.txt"
-	fi
-	sed 1d "csvs/$filename/$bd.csv" >> "csvs/$filename.csv"
-        end=$(date -d"$end - 1 day" +"%Y%m%d")
-done
+if [[ $query == from:* ]]; then
+	echo "Running $query"
+	twint -s "$query" --csv --output "csvs/$filename.csv" --since "$bd" --until "$ed" --proxy-host tor >> "logs/$filename.txt"
+else
+	while [[ $start -lt $end ]]
+	do
+		bd=$(date -d"$end - 1 day" +"%Y-%m-%d")
+		ed=$(date -d"$end" +"%Y-%m-%d")
+		if [ -f "csvs/$filename/$bd.csv" ]; then
+			echo "$bd already done, skipping"
+		else
+				echo "Running $query for $bd"
+				twint -s "$query" --csv --output "csvs/$filename/$bd.csv" --since "$bd" --until "$ed" --proxy-host tor >> "logs/$filename.txt"
+		fi
+		sed 1d "csvs/$filename/$bd.csv" >> "csvs/$filename.csv"
+			end=$(date -d"$end - 1 day" +"%Y%m%d")
+	done
+fi
